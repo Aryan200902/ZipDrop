f4b24b6b5b9410f9d745d9c4cb18ccd9ce81e60c
Aryan Shrivastava <aryanshrivastava987@gmail.com>
2024-06-04 01:11:22 +0530

    Added Sample files

--- core/serializers.py
+++ core/serializers.py
@@ -9,7 +9,7 @@ class FileSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class FileListSerializers(serializers.Serializer):
+class FileListSerializer(serializers.Serializer):
     files = serializers.ListField(
         child = serializers.FileField(max_length = 100000, allow_empty_file = False , use_url = False)
     )

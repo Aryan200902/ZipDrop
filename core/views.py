250930b32b719de77ae9f015a2a69dc348b7894b
Aryan Shrivastava <aryanshrivastava987@gmail.com>
2024-06-03 19:29:12 +0530

    First Commit

---
+++ core/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser
+from .serializers import *
+
+def home(request):
+    return render(request,'home.html')
+
+def download(request, uid):
+    return render(request, 'download.html',context = {'uid': uid})
+
+class HandleFileUpload(APIView):
+    parser_classes = [MultiPartParser]
+    def post(self,request):
+        try:
+            data=request.data
+            
+            serializer = FileListSerializers(data=data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    'status' : 200,
+                    'message' : 'Files Uploaded Successfully',
+                    'data' : serializer.data
+                })
+
+            return Response({
+                'status' : 400,
+                'message' : 'Something Went Wrong',
+                'data' : serializer.errors
+            })
+        except Exception as e:
+            print(e)
+            
